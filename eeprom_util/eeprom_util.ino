6744b528de74b6da259d5b27f8a89a937277853e
topquark22 <topquark22@yahoo.ca>
2023-08-24 19:37:29 -0400

    EEPROM read utility

---
+++ eeprom_util/eeprom_util.ino
@@ -0,0 +1,17 @@
+#include <EEPROM.h>
+
+void setup() {
+  Serial.begin(9600);
+  for (int i = 0; i < 0x400; i++) {
+    byte c = EEPROM.read(i);
+    Serial.print(i);
+    Serial.print("\t");
+    Serial.print("0x");
+    Serial.print(c < 0x10 ? "0" : "");
+    Serial.print(c, HEX);
+    Serial.print("\t");
+    Serial.println((char)c);
+  }
+  delay(100);
+  exit(0);
+}
