b27e390381e21b85f37f05b44d6a1beb8abcc68d
topquark22 <topquark22@yahoo.ca>
2023-08-29 19:09:10 -0400

    more elegant

--- morse/morse.cpp
+++ morse/morse.cpp
@@ -105,14 +105,13 @@ void printMessage() {
 
 void readMessageFromEEPROM() {
   Serial.println("DEBUG readMessageFromEEPROM()");
-  int i = 0;
+  message_len = 0;
   byte b = EEPROM.read(0);
   while (b != 0 && i < MESSAGE_SIZE - 1) {
-    message[i] = b;
-    b = EEPROM.read(++i);
+    message[message_len] = b;
+    b = EEPROM.read(++message_len);
   }
-  message[i] = 0;
-  message_len = i;
+  message[message_len] = 0;
 }
 
 void writeIntToEEPROM(int addr, int value) {
