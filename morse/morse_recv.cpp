8b19326625b5c70343839bf6c98bb89627cc2ea5
topquark22 <topquark22@yahoo.ca>
2024-01-23 08:38:27 -0500

    refactor

--- morse/morse_recv.cpp
+++ morse/morse_recv.cpp
@@ -56,7 +56,7 @@ void receiveMessage() {
 
 void loop_RECV() {
   if (radio.available()) {
-    setErrorIndicator(false);
+    setErrorIndicator(LOW);
     radio.read(commBuffer, COMM_BUFFER_SIZE); // Read data from the nRF24L01
     switch (commBuffer[0]) {
       case TOKEN_MESSAGE: {
@@ -83,7 +83,7 @@ void loop_RECV() {
       }
       default: { // invalid token in commBuffer[0]
         Serial.println(F("Invalid packet received"));
-        setErrorIndicator(true);
+        setErrorIndicator(HIGH);
         delay(100); // wait to clear serial buffer
         exit(1);
       }
